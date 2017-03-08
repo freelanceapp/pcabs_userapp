afc9cf44241651fd02e20a0515875bb3e74bd799
Keerthivasan Sekar <s.keerthivasan@kfinsolutions.com>
2017-03-08 11:17:20 +0530

    Added My Location layer in google map.

--- js/controllers/land/landCtrl.js
+++ js/controllers/land/landCtrl.js
@@ -22,6 +22,7 @@ App.controller('landCtrl', function($scope,$rootScope,$q, $ionicLoading, $compil
 			  
 			  disableDefaultUI: true,
 			  mapTypeId: google.maps.MapTypeId.ROADMAP
+			  mMap.setMyLocationEnabled(true);
 			};
 			
 			var map = new google.maps.Map(document.getElementById("map"),
